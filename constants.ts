f14e90c401b21569c137b8097f636d7480749a6f
Yash Badgujar <yashbadgujar5415@gmail.com>
2023-11-30 10:04:41 +0530

    initial commit

---
+++ constants.ts
@@ -0,0 +1,46 @@
+import { Code, ImageIcon, MessageSquare, Music, VideoIcon } from 'lucide-react';
+
+export const MAX_FREE_COUNTS = 10;
+
+export const tools = [
+  {
+    label: 'Conversation',
+    icon: MessageSquare,
+    href: '/conversation',
+    color: 'text-violet-500',
+    bgColor: 'bg-violet-500/10',
+    isBeta: false,
+  },
+  {
+    label: 'Music Generation',
+    icon: Music,
+    href: '/music',
+    color: 'text-emerald-500',
+    bgColor: 'bg-emerald-500/10',
+    isBeta: true,
+  },
+  {
+    label: 'Image Generation',
+    icon: ImageIcon,
+    color: 'text-pink-700',
+    bgColor: 'bg-pink-700/10',
+    href: '/image',
+    isBeta: false,
+  },
+  {
+    label: 'Video Generation',
+    icon: VideoIcon,
+    color: 'text-orange-700',
+    bgColor: 'bg-orange-700/10',
+    href: '/video',
+    isBeta: true,
+  },
+  {
+    label: 'Code Generation',
+    icon: Code,
+    color: 'text-green-700',
+    bgColor: 'bg-green-700/10',
+    href: '/code',
+    isBeta: false,
+  },
+];
